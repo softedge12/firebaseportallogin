eb54285862783b50c2a24d8281dd132f22b6dc40
softedge12 <softedgesoftwares12@gmail.com>
2025-02-07 15:30:37 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyA2xKyrBV9rO8vVMfzvxPMUAfV19y6eTeQ",
-  authDomain: "educationalwebsite-2247d.firebaseapp.com",
-  databaseURL: "https://educationalwebsite-2247d-default-rtdb.firebaseio.com",
-  projectId: "educationalwebsite-2247d",
-  storageBucket: "educationalwebsite-2247d.firebasestorage.app",
-  messagingSenderId: "711744017075",
-  appId: "1:711744017075:web:11429b93a8767d9b2556c6",
-  measurementId: "G-V7CPBP1TZ7"
+ apiKey: "AIzaSyBmmKkfC7S8GJvAlTQ7YjwKKPIsps-MUPk",
+  authDomain: "loginportal-48b5e.firebaseapp.com",
+  databaseURL: "https://loginportal-48b5e-default-rtdb.firebaseio.com",
+  projectId: "loginportal-48b5e",
+  storageBucket: "loginportal-48b5e.firebasestorage.app",
+  messagingSenderId: "566180631348",
+  appId: "1:566180631348:web:55415b14542e61557a8b61"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
