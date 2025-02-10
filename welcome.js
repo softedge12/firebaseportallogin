cc1a50ba137bf49a49ba2349b9d49fcf54d41a62
softedge12 <softedgesoftwares12@gmail.com>
2025-02-10 13:25:17 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,12 +1,36 @@
-firebase.auth().onAuthStateChanged((user)=>{
-    if(!user){
-        location.replace("index.html")
-    }else{
-        document.getElementById("user").innerHTML = "Hello, "+user.email
+firebase.auth().onAuthStateChanged((user) => {
+    if (!user) {
+        location.replace("index.html");
+    } else {
+        document.getElementById("user").innerHTML = "Hello, " + user.email;
+        checkExpiry(user);
     }
-})
+});
 
+function logout() {
+    firebase.auth().signOut();
+}
 
-function logout(){
-    firebase.auth().signOut()
+function checkExpiry(user) {
+    const userEmail = user.email;
+
+    firebase.database().ref("users").orderByChild("email").equalTo(userEmail).once("value")
+        .then(snapshot => {
+            if (snapshot.exists()) {
+                snapshot.forEach(userData => {
+                    const expiryDate = new Date(userData.val().expiryDate);
+                    const currentDate = new Date();
+
+                    if (currentDate > expiryDate) {
+                        alert("Your account has expired. Please contact support.");
+                        firebase.auth().signOut().then(() => {
+                            location.replace("index.html");
+                        });
+                    }
+                });
+            }
+        })
+        .catch(error => {
+            console.error("Error checking expiry:", error.message);
+        });
 }
